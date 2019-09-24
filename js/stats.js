27c92ce36c011334862cd4d7f3d3827103679ac2
Ashray Shetty <Ashray.Shetty@empired.com>
2019-09-24 17:52:40 +1000

    more cleanup

--- js/stats.js
+++ js/stats.js
@@ -1,59 +1,54 @@
-getSteamGames = async () => {
-	var xhttp = new XMLHttpRequest();
-
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			// Typical action to be performed when the document is ready:
-			var response = JSON.parse(xhttp.responseText);
-			document.getElementById('game_name').innerHTML = response.response.games[0].name;
-			document.getElementById('game_time').innerHTML = response.response.games[0].playtime_forever / 60;
-		}
-	};
-	xhttp.open('GET', 'http://ashraystats.azurewebsites.net/steam/latestgames', true);
-	xhttp.send();
-};
-
 calculateAge = () => {
 	// birthday is a date
-	var birthday = new Date('20 Dec 1996');
-	var curr = Date.now();
-	var diff = curr - birthday;
+	const birthday = new Date('20 Dec 1996');
+	const curr = Date.now();
+	const diff = curr - birthday;
 
-	var age = (diff / 31557600000).toFixed(7); // Divide by 1000*60*60*24*365.25
+	const age = (diff / 31557600000).toFixed(7); // Divide by 1000*60*60*24*365.25
 	document.getElementById('my_age').innerHTML = age;
 };
 
 getRank = async () => {
-	var xhttp = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			// Typical action to be performed when the document is ready:
-			//Bronze, Silver, Gold, Diamond, Platinum, Master, Challenger
-			const { tier, rank } = JSON.parse(xhttp.responseText)[0];
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const { tier, rank } = JSON.parse(xhr.responseText)[0];
 			document.getElementById('tier').innerHTML = tier;
 			document.getElementById('rank').innerHTML = rank;
 		}
 	};
-	xhttp.open('GET', 'http://ashraystats.azurewebsites.net/lol/rank', true);
-	xhttp.send();
+	xhr.open('GET', 'http://ashraystats.azurewebsites.net/lol/rank', true);
+	xhr.send();
 };
 
 getMovies = async () => {
-	var xhttp = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			// Typical action to be performed when the document is ready:
-			// Bronze, Silver, Gold, Diamond, Platinum, Master, Challenger
-			const response = JSON.parse(xhttp.responseText);
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const response = JSON.parse(xhr.responseText);
 			document.getElementById('movie_1').innerHTML = response[0].Title;
 			document.getElementById('movie_2').innerHTML = response[1].Title;
 			document.getElementById('movie_3').innerHTML = response[2].Title;
 		}
 	};
-	xhttp.open('GET', 'http://ashraystats.azurewebsites.net/cinebuzz/getpastbookings', true);
-	xhttp.send();
+	xhr.open('GET', 'http://ashraystats.azurewebsites.net/cinebuzz/getpastbookings', true);
+	xhr.send();
+};
+
+getSteamGames = async () => {
+	let xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const response = JSON.parse(xhr.responseText);
+			document.getElementById('game_name').innerHTML = response.response.games[0].name;
+			document.getElementById('game_time').innerHTML = response.response.games[0].playtime_forever / 60;
+		}
+	};
+	xhr.open('GET', 'http://ashraystats.azurewebsites.net/steam/latestgames', true);
+	xhr.send();
 };
 
 $(document).ready(() => {
